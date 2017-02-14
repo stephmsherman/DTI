fc14a9a5ba44765ad7adc31debe345ab9bda8ede
Stephanie Sherman <stephmsherman@gmail.com>
2017-02-14 15:04:43 -0500

    additions

--- get_covariates.py
+++ get_covariates.py
@@ -1,14 +1,18 @@
 import glob, sys, os
 import pandas as pd
 import numpy as np
- 
-file = glob.glob('path/all_data_????-??-??.csv')
+
+dti_folder = '/home3/kensinel/fMRI_DATA/SleepLDF/DTI/'
+file = glob.glob('/home3/kensinel/fMRI_DATA/SleepLDF/combined_data/all_data_????-??-??.csv')
 
 ##read in data
 d = pd.read_csv(file[0])
 
+##change SleepLDF to SLEEPLDF in subject names
+d['subject'] = d['subject'].str.replace('Sleep','SLEEP') 
+
 ###remove 2 participants who do not have DTI data
-d = d[(d.subject != 'SleepLDF004') & (d.subject != 'SleepLDF006')]
+d = d[(d.subject != 'SLEEPLDF004') & (d.subject != 'SLEEPLDF006')]
 
 ##create column of ones for design matrix
 d['one'] = 1
@@ -23,15 +27,21 @@ m = d[['subject', 'one' , covariate]]
 m = m[np.isfinite(m[covariate])]
 
 subjects_included = m.subject
-subjects_included.replace('Sleep', 'SLEEP',regex=True)
+
+os.mkdir(dti_folder + covariate)
+
+for subjects in subjects_included:
+	#
 
 ##get number of participants
 num = m.shape[0]
 
-f = open(('path/design_%s.mat' % covariate), 'w')
+mat = m[['one', covariate]]
+
+f = open(('/Volumes/LabShareFolder/SleepLDF/Combine_all_data/design_%s.mat' % covariate), 'w')
 f.write('/NumWaves 2\n')
 f.write('/NumPoints %d\n' % num)
 f.write('/PPheights 1 1\n')
 f.write('/Matrix\n')
-m.to_csv(f, sep = " ",header=False,index=False)
+mat.to_csv(f, sep = " ",header=False,index=False)
 f.close()
